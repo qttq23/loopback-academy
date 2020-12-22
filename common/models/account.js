d3dd913ec3c0ce83d5a706bd9cc97a642ae84ac8
qttq23 <1753102@student.hcmus.edu.vn>
2020-12-22 21:02:11 +0700

    validate data & relation

--- common/models/account.js
+++ common/models/account.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = function(Account) {
+
+	// limit api
 	Account.disableRemoteMethodByName('create');
 	Account.disableRemoteMethodByName('prototype.patchAttributes');
 	Account.disableRemoteMethodByName('deleteById');
+
+	// validation
+	Account.validatesUniquenessOf('email', { message: 'email is not unique' });
 };
--- common/models/course.js
+++ common/models/course.js
@@ -2,4 +2,8 @@
 
 module.exports = function(Course) {
 
+	// https://loopback.io/doc/en/lb3/Validating-model-data.html
+	//https://loopback.io/doc/en/lb3/MongoDB-connector.html#handling-objectid
+	Course.validatesLengthOf('name', { min: 5, max: 20 });
+
 };
