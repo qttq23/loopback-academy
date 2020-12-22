26c8fc5dbda3d96bb5b477b37d3ecd568a86d0e0
qttq23 <1753102@student.hcmus.edu.vn>
2020-12-22 20:31:02 +0700

    limit api, limit account: only read

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Account) {
-
+	Account.disableRemoteMethodByName('create');
+	Account.disableRemoteMethodByName('prototype.patchAttributes');
+	Account.disableRemoteMethodByName('deleteById');
 };
